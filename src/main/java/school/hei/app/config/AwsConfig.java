28472574d00e32493c0f8599195202b615b157c5
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-07-08 22:25:11 +0300

    feat: add S3 with IAM role (no keys)

--- src/main/java/school/hei/app/config/AwsConfig.java
+++ src/main/java/school/hei/app/config/AwsConfig.java
@@ -3,8 +3,6 @@ package school.hei.app.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -12,30 +10,16 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class AwsConfig {
 
-  @Value("${aws.access-key:}")
-  private String accessKey;
-
-  @Value("${aws.secret-key:}")
-  private String secretKey;
-
   @Value("${aws.region:eu-west-3}")
   private String region;
 
   @Bean
   public S3Client s3Client() {
-    return S3Client.builder()
-        .region(Region.of(region))
-        .credentialsProvider(
-            StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-        .build();
+    return S3Client.builder().region(Region.of(region)).build();
   }
 
   @Bean
   public S3Presigner s3Presigner() {
-    return S3Presigner.builder()
-        .region(Region.of(region))
-        .credentialsProvider(
-            StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-        .build();
+    return S3Presigner.builder().region(Region.of(region)).build();
   }
 }
